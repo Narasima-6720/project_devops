c9c10166bdaa0549800ecfa5445162c269c66d63
Madanapalli Narasimha Reddy <159816623+Narasima-6720@users.noreply.github.com>
2024-07-24 11:11:10 +0530

    Delete readqrcode.py

--- readqrcode.py
+++
@@ -1,7 +0,0 @@
-import cv2
-def qrread(image):
-    img=cv2.imread(image)
-    detector=cv2.QRCodeDetector()
-    data , bbox , _ = detector.detectAndDecode(img)
-    return data
-print(qrread('QR Code.png'))
